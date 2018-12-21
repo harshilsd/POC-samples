dbc3ed55fa89c89be13192d48be68b15e8ddb96d
harshilsd <46040943+harshilsd@users.noreply.github.com>
2018-12-21 16:13:59 +0530

    modified path

--- src/main/java/com/test/sd/HelloController.java
+++ src/main/java/com/test/sd/HelloController.java
@@ -3,14 +3,16 @@ package com.test.sd;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("")
 @Controller
+@RequestMapping("harshil")
 public class HelloController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model){
 
-        model.addAttribute("message","Hello Cow");
+        model.addAttribute("message","Hello Cow!");
         return "hello";
     }
 }
